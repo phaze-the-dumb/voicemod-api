4d6719f384aad45c49a40cbdec874cff55aac8c9
phaze-the-dumb <phaze@duck.com>
2023-08-02 13:02:54 +0100

    Add tunaauth

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 // TunaAPI
 const TunaAPI = require('./TunaAPI');
 
+// TunaAuth
+const TunaAuth = require('./TunaAuth');
+
 // VoiceMod API
 const VoiceMod = require('./VoiceMod');
 
-module.exports = { VoiceMod, TunaAPI };
+module.exports = { VoiceMod, TunaAPI, TunaAuth };
